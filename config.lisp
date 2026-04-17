e8a87d9ac29808c844abbf55cdc2a620ffd5fcc1
M <matf@disr.it>
2026-04-18 00:48:21 +0200

    Whoopsie, forgot config.lisp.

--- config.lisp
+++ config.lisp
@@ -1,6 +1,7 @@
 (in-package :nyxt-user)
 
 ;; Base
+(nyxt::load-lisp "~/.config/nyxt/misc/guix-fix.lisp")
 (nyxt::load-lisp "~/.config/nyxt/theme/catppuccin.lisp")
 (nyxt::load-lisp "~/.config/nyxt/theme/catppuccin-flavour.lisp")
 (nyxt::load-lisp "~/.config/nyxt/base/urlprompt.lisp")
